48e59d14e40ed6104f6606ec9a6065bf07b91c73
rvkrishna268 <rvkrishna268@gmail.com>
2022-09-19 17:04:07 +0530

    Added schema for propertyModel

--- server/model/property.js
+++ server/model/property.js
@@ -1,13 +1,43 @@
 const mongoose = require('mongoose');
 
-// Schema for property
+const propertySchema=new mongoose.Schema({
+    propertyType: {type:String},
+    negotiable:{type:String},
+    price: {type:Number},
+    ownership:{type:String},
+    propertyAge:{type:Number},
+    propertyApproved:{type:String},
+    propertyDescription:{type:String},
+    bankLoan:{type:String},
+    length:{type:Number},
+    breath:{type:Number},
+    totalArea:{type:Number},
+    areaUnit:{type:String},
+    noOfBHK:{type:Number},
+    noOfFloor:{type:Number},
+    attached:{type:String},
+    westernToilet:{type:String},
+    furnished:{type:String},
+    carParking:{type:String},
+    lift:{type:String},
+    electricity:{type:String},
+    facing:{type:String},
+    name:{type:String},
+    mobile:{type:Number,minLength:10,maxLength:10},
+    postedBy:{type:String},
+    saleType:{type:String},
+    featuredPackage:{type:String},
+    PPDPackage:{type:String},
+    email:{type:String},
+    city:{type:String},
+    area:{type:String},
+    pincode:{type:Number,minLength:6,maxLength:6},
+    address:{type:String},
+    landmark:{type:String},
+    latitude:{type:String},
+    longitude:{type:String}
+})
 
+const propertyModel=mongoose.model("property",propertySchema);
 
-
-
-
-//Model for Property 
-
-
-
-// Export Model
+module.exports=propertyModel;
