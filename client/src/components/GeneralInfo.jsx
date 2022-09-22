c6e796f3695043de7422a5d11ad28fa659b776c4
rudissh-kanna <rudisshkanna.1199@gmail.com>
2022-09-22 15:58:42 +0530

    Minor changes in functionality of form

--- client/src/components/GeneralInfo.jsx
+++ client/src/components/GeneralInfo.jsx
@@ -37,12 +37,11 @@ const GeneralInfo = () => {
         </Grid>
         <Grid item xs={6}>
           <div
-            style={{ width: "92%", marginLeft: "4%" }}
-            value={formData.postedBy}
-            onChange={e =>
-              setFormData({ ...formData, postedBy: e.target.value })
-            }>
-            <TextField label="Posted By" select size="small">
+            style={{ width: "92%", marginLeft: "4%" }}>
+            <TextField label="Posted By" select size="small"
+                value={formData.postedBy}
+                onChange={e => setFormData({...formData, postedBy: e.target.value })
+                }>
               <MenuItem value="Owner">Owner</MenuItem>
               <MenuItem value="Tenent">Tenent</MenuItem>
             </TextField>
