0307561f157f372ee2f11bcce51b568afd09d317
rudissh-kanna <rudisshkanna.1199@gmail.com>
2022-09-24 13:45:10 +0530

    added the cors in index

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+var cors = require('cors');
 
 
 dotenv.config();
+app.use(cors());
 //connect to DB
 mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('connected to DB')
